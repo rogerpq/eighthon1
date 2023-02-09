d257d44ba0477c0af52fef10e89ebc3670791cf7
روجر <104784913+rogerpq@users.noreply.github.com>
2023-02-09 20:39:27 +0300

    FIX:[Config]:Reason:For easy ordering

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
-APP_ID = os.environ.get("APP_ID")
-APP_HASH = os.environ.get("APP_HASH")
+APP_ID = os.environ.get("API_ID")
+APP_HASH = os.environ.get("API_HASH")
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
 session = os.environ.get("TERMUX")
 SESSION = os.environ.get("TERMUX")
