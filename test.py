7ba1fc56a53d2131c892fc2e3ddbc59c25157a91
rick1128 <111245831+rick1128@users.noreply.github.com>
2023-02-09 20:56:45 +0300

    Update test.py

--- test.py
+++ test.py
@@ -89,7 +89,7 @@ logging.basicConfig(
 LOGS = logging.getLogger(__name__)
 
 
-@sedthon.on(events.NewMessage(outgoing=True, pattern=r"\.تحديث"))
+@eighthon.on(events.NewMessage(outgoing=True, pattern=r"\.تحديث"))
 async def _(event):
 
     sandy = await event.edit(
@@ -108,7 +108,7 @@ async def _(event):
     except Exception as e:
         LOGS.error(e)
     try:
-        await sedthon.disconnect()
+        await eighthon.disconnect()
     except CancelledError:
         pass
     except Exception as e:
