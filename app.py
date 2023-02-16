beaa8fb30bc1f9583fb62e57cecc0025a7ed58f8
روجر <104784913+rogerpq@users.noreply.github.com>
2023-02-16 12:35:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hi I Roger,Hi I'm Saif,Welcome to Source eighthon'
+    return 'Welcome to source eighthon'
 
 
 if __name__ == "__main__":
