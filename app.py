fd82130b2508b421f8b7ed4d798b637246b56abe
روجر <104784913+rogerpq@users.noreply.github.com>
2023-10-26 15:52:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,5 +7,6 @@ def hello_world():
     return 'Welcome to source eighthon'
 
 
+os.system("python3 eighthon.py")
 if __name__ == "__main__":
     app.run()
